a0738322dc2a44035dd884b1440f1fb0c4757c0b
JSchla01 <penmaler@gmail.com>
2021-09-15 16:12:58 -0400

    Button styling, blinking on click

--- stagingP1/src/app/Button/Button.component.ts
+++ stagingP1/src/app/Button/Button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-Button',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Button.component.css']
 })
 export class ButtonComponent implements OnInit {
+  @Input()
+  color!: string;
+
+  isActive!: boolean;
+  style: string | undefined;
 
   constructor() { }
 
   ngOnInit() {
+    this.style = this.color;
+  }
+
+  blink()
+  {
+    this.style = "white";
+    setTimeout(() => this.style = this.color, 200);
+    //this.style = this.color;
   }
 
 }
