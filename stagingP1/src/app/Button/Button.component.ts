4d833fb14b586f1385f8f596c8c6caa4cabf6d4e
JSchla01 <penmaler@gmail.com>
2021-09-16 17:20:29 -0400

    Button sound logic adjusted to 2 methods

--- stagingP1/src/app/Button/Button.component.ts
+++ stagingP1/src/app/Button/Button.component.ts
@@ -24,17 +24,139 @@ export class ButtonComponent implements OnInit {
   {
     this.style = "white";
     setTimeout(() => this.style = this.color, 200);
-    //this.style = this.color;
+    //this.select();
+    if (this.buttonId) {
+      this.selectedEvent.emit(this.buttonId)
+    }
+    //wait for input that this is the proper button
+    //Play appropriate sound based on input
+    //playSound plays button sound
+    //playFail plays failure sound
   }
 
-  select() {
-    let audio = <HTMLVideoElement>document.querySelector("#sound1");
-    console.log(audio);
+  playSound()
+  {
+    let audio = new Audio();
+
+    switch(this.color)
+    {
+      case "red" : {
+        audio.src = "/assets/sound/a.mp3";
+        console.log("a.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "blue" : {
+        audio.src = "/assets/sound/b.mp3";
+        console.log("b.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "yellow" : {
+        audio.src = "/assets/sound/c.mp3";
+        console.log("c.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "green" : {
+        audio.src = "/assets/sound/d.mp3";
+        console.log("d.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+    }
+  }
+
+  playFail()
+  {
+    let audio = new Audio();
+    audio.src = "/assets/sound/fail.mp3";
+    console.log("fail.mp3 playing");
     if (audio) {
       audio.play();
     }
-    if (this.buttonId) {
-      this.selectedEvent.emit(this.buttonId)
+  }
+
+  select() {
+    let audio = new Audio();
+
+    switch(this.color)
+    {
+      case "red" : {
+        audio.src = "/assets/sound/a.mp3";
+        console.log("a.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "blue" : {
+        audio.src = "/assets/sound/b.mp3";
+        console.log("b.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "yellow" : {
+        audio.src = "/assets/sound/c.mp3";
+        console.log("c.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      case "green" : {
+        audio.src = "/assets/sound/d.mp3";
+        console.log("d.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
+      default : {
+        audio.src = "/assets/sound/fail.mp3";
+        console.log("fail.mp3 playing");
+      if (audio) {
+        audio.play();
+      }
+      if (this.buttonId) {
+        this.selectedEvent.emit(this.buttonId)
+      }
+      break;
+      }
     }
   }
 }
